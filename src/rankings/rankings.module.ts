63b36aa04278507683b6e5d57a92053ac126e39b
Marlon <devmarlonferreira@gmail.com>
2024-02-27 15:55:37 -0300

    feat: Funcionalidade rankings

--- src/rankings/rankings.module.ts
+++ src/rankings/rankings.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RankingsController } from './rankings.controller';
-
+import { RankingSchema } from './interfaces/ranking.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RankingsService } from './rankings.service';
 @Module({
-  controllers: [RankingsController]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Ranking', schema: RankingSchema }]),
+  ],
+  providers: [RankingsService],
+  controllers: [RankingsController],
 })
 export class RankingsModule {}
--- src/rankings/rankings.service.ts
+++ src/rankings/rankings.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Partida } from './interfaces/partida.interface';
 
 @Injectable()
-export class RankingsService {}
+export class RankingsService {
+  private readonly logger = new Logger(RankingsService.name);
+
+  async processarPartida(idPartida: string, partida: Partida): Promise<void> {
+    this.logger.log(
+      `idPartida: ${JSON.stringify(idPartida)} - partida: ${JSON.stringify(partida)}`,
+    );
+  }
+}
