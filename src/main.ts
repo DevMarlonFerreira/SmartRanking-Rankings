e91f8b6a837280ab1bee0fd21c76502ef8607d88
Marlon <devmarlonferreira@gmail.com>
2024-03-01 11:43:12 -0300

    build: Add moment-timezone

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import * as momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
@@ -13,6 +14,12 @@ async function bootstrap() {
       queue: 'rankings',
     },
   });
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
 
   await app.listen();
 }
